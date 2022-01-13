1067be95718a2d366f47957f7dcd51d935198499
Mountain Scott <mountainscott@yahoo.com>
2022-01-12 22:28:25 -0800

    java banking application

--- Account.java
+++ Account.java
@@ -18,4 +18,29 @@ public class Account {
             previousTransaction = amount;
         }
     }
+
+    void withdraw(int amount) {
+        if(amount != 0) {
+            balance -= amount;
+            previousTransaction = -amount;
+        }
+    }
+
+    void getPreviousTransaction() {
+        if(previousTransaction > 0) {
+            System.out.println("Deposited: " + previousTransaction);
+        } else if(previousTransaction < 0) {
+            System.out.println("Withdrawn: " + Math.abs(previousTransaction));
+        } else {
+            System.out.println("No transaction was made");
+        }
+    }
+
+    void calculateInterest(int years) {
+        double interestRate = .0185;
+        double newBalance = (balance * interestRate * years) + balance;
+        System.out.println("The current interest rate is" + (100 * interestRate));
+        System.out.println("After " + years + " years, the balance is " + newBalance);
+    }
 }
+
