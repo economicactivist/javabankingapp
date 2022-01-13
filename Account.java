696f76b874950bc5ccc7f2c128e3ebe6a66a5761
Mountain Scott <mountainscott@yahoo.com>
2022-01-12 22:34:58 -0800

    added interest rate method

--- Account.java
+++ Account.java
@@ -42,5 +42,49 @@ public class Account {
         System.out.println("The current interest rate is" + (100 * interestRate));
         System.out.println("After " + years + " years, the balance is " + newBalance);
     }
+
+    void showMenu() {
+        char option = '\0';
+        Scanner input = new Scanner(System.in);
+        System.out.println("Welcome" + customerName + "!");
+        System.out.println("Enter 1 to make a deposit");
+        System.out.println("Enter 2 to make a withdrawal");
+        System.out.println("Enter 3 to see your previous transaction");
+        System.out.println("Enter 4 to calculate your interest");
+        System.out.println("Enter 5 to exit");
+        option = input.next().charAt(0);
+       
+        switch(option) {
+            case '1':
+                System.out.println("Enter the amount to deposit");
+                int amount = input.nextInt();
+                deposit(amount);
+                showMenu();
+                break;
+            case '2':
+                System.out.println("Enter the amount to withdraw");
+                amount = input.nextInt();
+                withdraw(amount);
+                showMenu();
+                break;
+            case '3':
+                getPreviousTransaction();
+                showMenu();
+                break;
+            case '4':
+                System.out.println("Enter the number of years");
+                int years = input.nextInt();
+                calculateInterest(years);
+                showMenu();
+                break;
+            case '5':
+                System.out.println("Thank you for banking with us!");
+                break;
+            default:
+                System.out.println("Invalid option");
+                showMenu();
+                break;
+        }
+    }
 }
 
